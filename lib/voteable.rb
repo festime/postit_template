63832f130b05c26b9a585ba56c688ad3ec7c9552
Yulin Chen <gloverock2@gmail.com>
2014-12-04 19:09:31 +0800

    extract module voteable to a gem

--- lib/voteable.rb
+++ lib/voteable.rb
@@ -1,23 +1,23 @@
-# using concerns
-module Voteable
-  extend ActiveSupport::Concern 
+# # using concerns
+# module Voteable
+#   extend ActiveSupport::Concern 
 
-  included do 
-    has_many :votes, as: :voteable
-  end
+#   included do 
+#     has_many :votes, as: :voteable
+#   end
 
-  def total_votes
-    up_votes - down_votes
-  end
+#   def total_votes
+#     up_votes - down_votes
+#   end
 
-  def up_votes
-    self.votes.where(vote: true).count
-  end
+#   def up_votes
+#     self.votes.where(vote: true).count
+#   end
 
-  def down_votes
-    self.votes.where(vote: false).count
-  end  
-end
+#   def down_votes
+#     self.votes.where(vote: false).count
+#   end  
+# end
 
 # using normal metaprogramming
 =begin
